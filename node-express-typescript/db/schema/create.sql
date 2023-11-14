7b814adea29e891189cb94f00c88fa0ea39cc41d
Henri William <henri@netengine.com.au>
2023-11-13 19:11:54 -0800

    added pg dependancy, setup db with user table and mocked user data

---
+++ node-express-typescript/db/schema/create.sql
@@ -0,0 +1,7 @@
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  phone VARCHAR(32),
+  password VARCHAR(255) NOT NULL
+);
---
+++ node-express-typescript/db/schema/development.sql
@@ -0,0 +1,5 @@
+INSERT INTO users (name, email, phone, password)
+VALUES
+('Egg Eggerson', 'friedEgg99@hotmail.com', '604789456', 'password'),
+('Micheal Schmidt', 'wilkinson.timmothy@hotmail.com', '778456123', 'password'),
+('Anderson Stokes', 'reichel_albina@stamm.info', '624123789', 'password');
